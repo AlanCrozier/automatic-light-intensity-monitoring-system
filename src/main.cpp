07105ccbff8b51f1ec28ea42563e5a595c883b9a
AlanCrozier <alan.crozier_cs.aiml24@gla.ac.in>
2026-02-04 10:03:16 +0530

    add pin to config main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@ void setup() {
 void loop() {
 // write your code here
     int ldrRaw = analogRead(ldrPin);
-    int lightPercent=map(ldrRaw, 0, 100, 0, 100);
-    Serial.println("LDR RAW");
+    int lightPercent=map(ldrRaw, 0, 1023, 0, 100);
+    Serial.println("LDR RAW :");
     Serial.println(ldrRaw);
-    Serial.println("Light PERCENT");
+    Serial.println("Light PERCENT : ");
     Serial.println(lightPercent);
     Serial.println("%");
     delay(1000);
